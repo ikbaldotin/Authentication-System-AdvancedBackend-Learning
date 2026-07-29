ee80f4e3bfe84b957d3fb27f955897292851ed23
Md Ikbal <ikbal84336@gmail.com>
2026-07-29 22:50:57 +0530

    add prefix refreshToken

--- backend/src/middleware/rate-limit/refresh-token-rate-limit.ts
+++ backend/src/middleware/rate-limit/refresh-token-rate-limit.ts
@@ -1,24 +1,23 @@
 import rateLimit, { ipKeyGenerator } from "express-rate-limit";
-import RedisStore from "rate-limit-redis";
-import redis from "../../lib/redis";
 
+import RedisStore from "rate-limit-redis";
+import redis from "../../lib/redis.js";
 export const refreshRateLimit = rateLimit({
-  windowMs: 5 * 60 * 1000,
-  max: 3,
+  windowMs: 10 * 1000, // for testing
+  max: 2, // for testing
   standardHeaders: true,
   legacyHeaders: false,
   store: new RedisStore({
     sendCommand: (...args: string[]) => {
       return redis.call(args[0], ...args.slice(1)) as Promise<any>;
     },
-    prefix:"refreshToken"
+    prefix: "refreshtoken",
   }),
-  handler: (_, res) => {
-    res.status(429).json({
+  handler: async (_, res) => {
+    return res.status(429).json({
       success: false,
-      message: "refresh rate limit,Too many request,pleaes try again later",
-    }),
-    
+      message: "login rate limit, Too many request,pleaes try again later",
+    });
   },
   keyGenerator: (req) => {
     return ipKeyGenerator(req.ip as string);
